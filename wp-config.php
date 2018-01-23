1b34b3e1abb8c8c77918eacc8cf4af3fed2cea44
Sergey <code.sergey.dolinin@gmail.com>
2018-01-23 13:20:38 +0300

    Initial Commit

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'thegirls_girls');
+define('DB_NAME', 'testgirls');
 
 /** MySQL database username */
-define('DB_USER', 'thegirls_girls');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'B7-uKPS-30');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
